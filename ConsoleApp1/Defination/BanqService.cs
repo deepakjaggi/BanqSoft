2a0312c617638f830b7d66643b6fbee26b6bdf23
V-PF0UT0HK\jaggidee <deepak.jaggi@gmail.com>
2018-02-03 17:19:06 +0100

    Ok

---
+++ ConsoleApp1/Defination/BanqService.cs
@@ -0,0 +1,16 @@
+﻿using ConsoleApp1.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Defination
+{
+
+    interface IBanqService
+    {
+        List<Payment> ReturnPayment(double TotalAmount, int NumberOfYears, double Interest);        
+        double GetInterest(int LoanType);
+        List<LoanType> GetLoanTypes();
+    }
+
+}
---
+++ ConsoleApp1/Models/LoanType.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Models
+{
+    class LoanType
+    {
+        private int loanType;
+        private String loanText;
+        public int getLoanType()
+        {
+            return loanType;
+        }
+        public void setLoanType(int loanType)
+        {
+            this.loanType = loanType;
+        }
+        public String getLoanText()
+        {
+            return loanText;
+        }
+        public void setLoanText(String loanText)
+        {
+            this.loanText = loanText;
+        }
+    }
+}
